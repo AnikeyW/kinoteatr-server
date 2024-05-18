66e46aff25d46978065372301c8f1e16a10e9427
AnikeeW <egor4ik048@gmail.com>
2024-05-18 22:39:40 +0300

    ..

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,8 @@ module.exports = {
   apps: [
     {
       name: 'holotv-server',
-      script: 'npm',
-      args: 'run start:prod',
+      script: 'node',
+      args: 'dist/main.js', // Убедитесь, что путь к main.js верный
       autorestart: true,
       watch: false,
       max_memory_restart: '1G',
